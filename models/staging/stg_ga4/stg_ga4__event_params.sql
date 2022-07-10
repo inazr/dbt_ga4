e14eafce0c9ccfe7ff7c21e1aa4b5de8b5b93f26
Stephan Ohm <github@stephan-ohm.com>
2022-07-10 15:27:44 +0200

    fixed join_key in stg_ga4__event_params.sql

--- models/staging/stg_ga4/stg_ga4__event_params.sql
+++ models/staging/stg_ga4/stg_ga4__event_params.sql
@@ -3,7 +3,7 @@ WITH event_params_cte AS (
     SELECT
             user_pseudo_id,
             case when event_params.key = 'ga_session_id' then event_params.value.int_value else null end as ga_session_id,
-            TIMESTAMP_MICROS(event_timestamp) AS event_timestamp,
+            event_timestamp,
             event_name,
             event_params.key,
             event_params.value.string_value,
@@ -26,7 +26,7 @@ WITH event_params_cte AS (
 SELECT
         MAX(ga_session_id) OVER (PARTITION BY event_timestamp, event_name) AS ga_session_id,
         user_pseudo_id,
-        event_timestamp,
+        TIMESTAMP_MICROS(event_timestamp) AS event_timestamp,
         event_name,
         key,
         string_value,

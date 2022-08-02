e23fd05ab28236d6377708acf7d3d5564118b02e
Stephan Ohm <github@stephan-ohm.com>
2022-08-02 08:38:28 +0200

    small fix in incremental strategy for int_ga4__session_landing_pages.sql

--- models/intermediate/int_ga4__session_landing_pages.sql
+++ models/intermediate/int_ga4__session_landing_pages.sql
@@ -9,13 +9,18 @@ WITH get_row_numbers_for_session_events AS (
     SELECT
             stg_ga4__event_params.ga_session_id,
             stg_ga4__event_params.event_timestamp,
-            stg_ga4__event_params.event_name,
             stg_ga4__event_params.string_value,
             ROW_NUMBER() over (PARTITION BY stg_ga4__event_params.ga_session_id ORDER BY stg_ga4__event_params.event_timestamp ASC) as row_number_per_session_id,
     FROM
             {{ ref('stg_ga4__event_params') }}
     WHERE   1=1
       AND   stg_ga4__event_params.key = 'page_location'
+
+    {% if is_incremental() %}
+
+      AND   stg_ga4__event_params.event_timestamp >= ( SELECT MAX( event_timestamp ) FROM {{ this }} )
+
+    {% endif %}
 
 )
 
@@ -28,8 +33,4 @@ FROM
 WHERE   1=1
   AND   row_number_per_session_id = 1
 
-        {% if is_incremental() %}
-  AND
-        stg_ga4__flat_events.event_timestamp >= ( SELECT MAX( event_timestamp ) FROM {{ this }} )
 
-        {% endif %}
